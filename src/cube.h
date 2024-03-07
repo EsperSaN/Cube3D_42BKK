918d8a478c3e6ab7988c9c6fae8a9bc758b62811
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-03-07 18:34:36 +0700

    fix main and add the st

--- src/cube.h
+++ src/cube.h
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:00:41 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/02/28 20:28:45 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/03/07 17:44:09 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,54 @@
 #endif
 #define INVALID_ARG "No argument supplied"
 #define FAIL 0
-#define SCREEN_HIGHT 720
-#define SCREEN_WIDTH 1280
+#define SCREEN_HIGHT 800
+#define SCREEN_WIDTH 800
+#define MINIMAPS_SCALE 20
 #define WIN_TITLE "RAYCASTING"
 
 #define W 13
 #define A 0
 #define S 1
 #define D 2
+#define LEFT_ARROW 123
+#define RIGHT_ARROW 124
 #define ESC 53
-#define PIXEL_SPEED 5
+#define PIXEL_SPEED 2
+#define ROTATE_SPEED 1
+#define PI 3.14
+
+
+typedef struct s_positon
+{
+	float	x;
+	float	y;
+}	t_pos;
+
+typedef struct s_vector
+{
+	unsigned int	magnitude;
+	t_pos			tail;
+	t_pos			head;
+	t_pos			unit;
+}	t_vector;
 
 typedef struct s_frame_image
 {
-	void *image;
-	char *address;
-	int bpp; // bit per pixel
-	int line_length;
-	int endian;
-	int end_row;
-	int start_row;
+	void	*image;
+	char	*address;
+	int		bpp; // bit per pixel
+	int		line_length;
+	int		endian;
+	int		frame_width;
+	int		frame_hight;
 }	t_frame;
 
 typedef struct s_texture_data
 {
-	char	*north;
-	char	*south;
-	char	*west;
-	char	*east;
+	char	**north;
+	char	**south;
+	char	**west;
+	char	**east;
 	int		floor[3];
 	int		ceil[3];
 }	t_texture;
@@ -57,22 +77,37 @@ typedef struct s_texture_data
 typedef struct s_map_data
 {
 	char		**maps_array;
+	int			maps_width;
+	int			maps_hight;
+	char		**raw_data;
 	t_texture	*texture;
 }	t_maps;
 
-typedef struct s_variable
+typedef struct s_keybind
 {
-	int		posx;
-	int		posy;
-	void	*mlx;
-	void	*game_window;
-	void	*data_window;
-	char	**raw_data;
 	int		w_key;
 	int		a_key;
 	int		s_key;
 	int		d_key;
-	t_maps	*maps_data;
+	int		left_key;
+	int		right_key;
+	int		is_keypress;
+}	t_key;
+
+typedef	struct s_player_data
+{
+	t_pos	pos;
+	float	angle;
+}	t_player ;
+
+typedef struct s_variable
+{
+	t_player	player;
+	void		*mlx;
+	void		*game_window;
+	void		*data_window;
+	t_key		key;
+	t_maps		*maps_data;
 }	t_data;
 
 # include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 16:38:38 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/02/28 20:38:41 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/03/07 15:46:22 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,39 @@
 
 int	main(int ac, char **av)
 {
-	t_data data;
+	t_data	data;
 
 	if(init_data(&data) == FAIL)
 		return (puterror("FAILL TO INIT DATA"), 0);
-	// data.maps_data = maps_parser(&data, av[1]);
-	data.posx = 1;
-	data.posy = 1;
+	data.maps_data = maps_parser(av[1]);
+	int i;
+	i = 0;
+	while (data.maps_data->maps_array[i])
+	{
+		int j = 0;
+		while (data.maps_data->maps_array[i][j] != NULL)
+		{
+			printf("%c",data.maps_data->maps_array[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	data.player.pos.x = 100;
+	data.player.pos.y = 100;
+	data.player.angle = 0;
+	data.key.is_keypress = 0;
+	data.maps_data->maps_hight = i--;
+	data.maps_data->maps_width = (ft_strlen(data.maps_data->maps_array[0]));
+	printf("w = %d h = %d\n" ,data.maps_data->maps_width ,data.maps_data->maps_hight);
+	printf("start init\n");
 	init_mlx(&data, SCREEN_HIGHT, SCREEN_WIDTH);
+	printf("init fin\n");
 	data.game_window = mlx_new_window(data.mlx, SCREEN_WIDTH, SCREEN_HIGHT, WIN_TITLE);
-	printf("mlx\nwin = %p\nptr = %p\n", data.mlx, data.game_window);
+	printf("data win with %d and %d\n",data.maps_data->maps_width, data.maps_data->maps_hight);
+	data.data_window = mlx_new_window(data.mlx, data.maps_data->maps_width * MINIMAPS_SCALE, data.maps_data->maps_hight * MINIMAPS_SCALE, "data");
+	printf("init fin\n");
 	control(&data);
 	mlx_loop_hook(data.mlx, render_frame, &data);
-	//render_frame(&data);
 	mlx_loop(data.mlx);
 }
