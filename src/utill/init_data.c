a3b23b5f94f84884e07c5d885b8eae42eac801d9
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-03-07 18:35:06 +0700

    ch the init

--- src/utill/init_data.c
+++ src/utill/init_data.c
@@ -6,7 +6,7 @@
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:10:40 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/02/28 21:02:56 by pruenrua         ###   ########.fr       */
+/*   Updated: 2024/03/04 23:32:21 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int		init_mlx(t_data *data, int screen_hight, int screen_width)
 {
 	data->mlx = mlx_init();
-	//data->game_window = mlx_new_window(data->mlx, screen_hight, screen_width, WIN_TITLE);
 }
 
 int	init_data(t_data *data)
@@ -23,11 +22,12 @@ int	init_data(t_data *data)
 	data->mlx = NULL;
 	data->game_window = NULL;
 	data->data_window = NULL;
-	data->raw_data = NULL;
-	data->a_key = 0;
-	data->s_key = 0;
-	data->d_key = 0;
-	data->w_key = 0;
+	data->key.a_key = 0;
+	data->key.s_key = 0;
+	data->key.d_key = 0;
+	data->key.w_key = 0;
+	data->key.left_key = 0;
+	data->key.right_key = 0;
 	data->maps_data = ft_calloc(sizeof(t_maps), 1);
 	data->maps_data->texture = ft_calloc(sizeof(t_texture), 1);
 	if (data->maps_data == NULL || data->maps_data->texture == NULL)
