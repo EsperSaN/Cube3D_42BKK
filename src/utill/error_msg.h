1bc34d029eecbced9ea8600fe4d58cbbaff840e6
ESZU <57306907+EsperSaN@users.noreply.github.com>
2024-03-17 00:07:35 +0700

    add report and util

--- src/utill/error_msg.h
+++ src/utill/error_msg.h
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utill.h                                            :+:      :+:    :+:   */
+/*   error_msg.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pruenrua <pruenrua@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 12:04:15 by pruenrua          #+#    #+#             */
-/*   Updated: 2024/03/15 22:28:04 by pruenrua         ###   ########.fr       */
+/*   Created: 2024/03/11 12:08:58 by pruenrua          #+#    #+#             */
+/*   Updated: 2024/03/11 12:09:34 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILL_H
-# define UTILL_H
+#ifndef ERROR_MSG_H
+# define ERROR_MSG_H
 
-# include "../cube.h"
-# include "./error_msg.h"
 
-void	puterror(char *error_msg);
-void	putreport(char *msg);
-float	get_rad(float deg);
-int		get_rgb(int r, int g, int b, int a);
-void	print_maps_data(char **maps);
-int		init_data(t_data *data, char *maps_file);
-int		clear_close(t_data *d);
+# define NO_ARG "No argument supplied"
+# define MLX_FAIL "MLX_INIT FAIL"
+# define PARSER_FAIL "FILE_PASER FAIL"
+# define INIT_FAIL "INIT FAIL"
+
+
+
 #endif
